76ed83f390960a33e19485504a75051eb510509c
azizulloyev <arabictoplegend@gmail.com>
2024-02-09 17:48:06 +0000

    js 1-dars

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+var name1 = prompt('ismingizni kirting')
+var age2 = prompt('yoshingizni kiriting')
+var tel3 = prompt('telefon raqimingizni kiritng')
+console.log('ismi ' + name1);
+console.log('yoshi ' + age2);
+console.log('tel raqami ' + tel3);
